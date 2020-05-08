8401e8666766e16473848f013b57e0eda3c29073
KyryliukAnton <kurulykantonua@gmail.com>
2020-05-08 12:24:35 +0300

    Add delete numbers and validatin

--- js/base.js
+++ js/base.js
@@ -1,67 +1,78 @@
 let choose_number = 0;
 let element;
+let play;
+let play_start;
 
 // Each array is a column (index 0 - column 1, index 1 - column 2, ... , index 8 - column 9).
 let field = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [4, 5, 6, 7, 8, 9, 1, 2, 3], [7, 8, 9, 1, 2, 3, 4, 5, 6],
     [2, 3, 4, 5, 6, 7, 8, 9, 1], [5, 6, 7, 8, 9, 1, 2, 3, 4], [8, 9, 1, 2, 3, 4, 5, 6, 7],
     [3, 4, 5, 6, 7, 8, 9, 1, 2], [6, 7, 8, 9, 1, 2, 3, 4, 5], [9, 1, 2, 3, 4, 5, 6, 7, 8]];
 
-let play;
-
-// $('.wrapper-input-cell input').on('input', function() {
-//     if(!$(this).val().match(/^[1-9]$/)){
-//         $(this).val($(this).val().slice(0,-1));
-//     }
-// });  
-
-function chooseNumber(el) {
-
-    if (element) {
-        $(element).text($(el).text());
-    }
-
-//     if ($(element).text()!=choose_number){
-//     choose_number=$(element).text();
-//   //  $(element).css('background','#CDDAC8')
-//     }
-//     else {
-//         choose_number=0;
-//     }
-}
-
 $('.cell_number').click(function () {
     let id = $(this).attr("id");
-    if (play[id[5]][id[7]] !== 0) {
+    if (play_start[id[5]][id[7]] !== 0) {
         return;
     }
-    console.log(id);
     if (element) {
         $(element).css('border', 'none');
     }
     element = this;
     $(element).css('border', '2px solid #ffffff');
-
-    // if(choose_number){
-    //     $(this).text(choose_number);
-    // }
 });
 
-$().ready(function () {
-    //let cells = $("div.cell_number");
-
-    swap_numbers();
+$().ready(function () { 
     // Fill in the field with numbers.
-    play = field;
-
+    play_start = copyArray(field);
+    swap_numbers();
+    // Remove a certain number of numbers.
+    remove_numbers(50);
+    play = copyArray(play_start);
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
-            if (play[i][j] != 0) {
-                let id = '#cell_' + i + '_' + j;
-                $(id).text(play[i][j]);
+            let id = '#cell_' + i + '_' + j;
+            if (play_start[i][j] != 0) {
+                $(id).text(play_start[i][j]);
+            }
+            else {
+                $(id).addClass('input_number');
             }
         }
     }
 });
+
+function copyArray (arr) {
+    let new_array=new Array;
+    for (let i = 0; i < arr.length; i++) {
+        new_array.push(arr[i].slice());
+    }
+    return new_array;
+}
+
+function chooseNumber(el) {
+    if (element) {
+        $(element).text($(el).text());
+        play[element.id[5]][element.id[7]] = parseInt($(el).text());
+        for (let i = 0; i < 9; i++) {
+            if(play[i].includes(0)) {
+                return;
+            }
+        }
+
+        if(validation()) {
+            console.log(true);
+        }
+        else {
+            console.log(false);
+        }
+    }
+}
+
+function deleteNumber() {
+    if (element) {
+        $(element).text('');
+        play[element.id[5]][element.id[7]]=0;
+    }
+}
 
 // Choosing a method for swap numbers 
 function swap_numbers() {
@@ -94,17 +105,17 @@ function swap_numbers() {
 
 // Method for swap columns
 function swap_columns(index_1, index_2) {
-    let temporary = field[index_1];
-    field[index_1] = field[index_2];
-    field[index_2] = temporary;
+    let temporary = play_start[index_1];
+    play_start[index_1] = play_start[index_2];
+    play_start[index_2] = temporary;
 }
 
 // Method for swap rows.
 function swap_rows(index_1, index_2) {
     for (let index = 0; index < 9; index++) {
-        let temporary = field[index][index_1];
-        field[index][index_1] = field[index][index_2];
-        field[index][index_2] = temporary;
+        let temporary = play_start[index][index_1];
+        play_start[index][index_1] = play_start[index][index_2];
+        play_start[index][index_2] = temporary;
     }
 }
 
@@ -121,6 +132,58 @@ function swap_three_columns(sector_1, sector_2) {
         swap_columns(sector_1, sector_2);
     }
 }
+
+// Remove a certain number of numbers.
+function remove_numbers(count=40) {
+    let cells = new Array;
+    for (let i=0; i<9; i++) {
+       for (let j=0; j<9; j++) {
+            cells.push(i.toString() + j.toString());
+       }
+    }
+    
+    while(count>0) {
+        count--;
+        let position=getRandomInt(0, cells.length-1);
+        play_start[cells[position][0]][cells[position][1]]=0;
+        cells.splice(position, 1);
+    }
+}
+
+function validation() {
+    let valid=[0,0,0,0,0,0,0,0,0];
+
+    for(let i=0; i<9; i++) {
+        let valid_row=valid.slice();
+        let valid_column=valid.slice();
+        for (let j=0; j<9; j++) {
+            valid_column[play[i][j]-1]++;
+            valid_row[play[j][i]-1]++;
+        }
+
+        if(valid_column.includes(0) || valid_row.includes(0)) {
+            return false;
+        }
+    }
+
+    for(let i=0; i<9; i+=3) {
+        let valid_square=valid.slice();
+        for (let j=0; j<9; j++) {
+            for (let k=0; k<3; k++) {
+                valid_square[play[i][j+k]-1]++;
+            }
+            if ((j+1)%3 === 0) {
+                if (valid_square.includes(0)) {
+                    return false;
+                }
+                else {
+                    let valid_square=valid.slice();
+                }
+            }
+        }
+    }
+    return true;
+} 
 
 // Get a random number from a range (including borders)
 function getRandomInt(min, max) {
